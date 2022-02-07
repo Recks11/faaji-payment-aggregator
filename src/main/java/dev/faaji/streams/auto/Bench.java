a9a56387b416e4883b7c8dbff9f6b314e6df63dc
Rex Ijiekhuamen <dev.rexijie@gmail.com>
2022-02-07 11:40:33 +0000

    updated cloud configuration

--- src/main/java/dev/faaji/streams/auto/Bench.java
+++ src/main/java/dev/faaji/streams/auto/Bench.java
@@ -7,9 +7,7 @@ import dev.faaji.streams.service.bindings.StreamBindings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.Random;
