cb21f108e987430df8002bcb900560d124bfdc9d
Rex Ijiekhuamen <dev.rexijie@gmail.com>
2022-02-11 19:11:28 +0000

    Fixed matching algorithm

--- src/main/java/dev/faaji/streams/auto/Bench.java
+++ src/main/java/dev/faaji/streams/auto/Bench.java
@@ -10,7 +10,9 @@ import dev.faaji.streams.service.bindings.StreamBindings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -18,8 +20,8 @@ import java.util.Random;
 import java.util.UUID;
 import java.util.stream.Stream;
 
-//@Component
-//@EnableScheduling
+@Component
+@EnableScheduling
 public class Bench {
     private static final
     Logger LOG = LoggerFactory.getLogger(Bench.class);
@@ -29,12 +31,23 @@ public class Bench {
         this.producer = producer;
     }
 
-//    @Scheduled(fixedDelay = 8000L)
+    @Scheduled(fixedDelay = 8000L)
     public void produceIndefinitely() {
         try {
             var event = new PartyModificationEvent(
                     "CREATE",
-                    new PartyModification("9qwyfibaoeeqr77")
+                    new PartyModification("6206989d921c1500161a7487")
+            );
+            producer.send("valentines-parties", event.getData().getEventId(), event);
+            LOG.info("Emitted %s".formatted(event));
+        } catch (Exception ex) {
+            LOG.error(ex.getMessage());
+        }
+
+        try {
+            var event = new PartyModificationEvent(
+                    "CREATE",
+                    new PartyModification("6202d1a4a891210016548311")
             );
             producer.send("valentines-parties", event.getData().getEventId(), event);
             LOG.info("Emitted %s".formatted(event));
