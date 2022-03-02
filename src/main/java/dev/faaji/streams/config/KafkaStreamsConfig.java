18451f0dd6b820ad3265040a8f670fafe724beb1
Rex Ijiekhuamen <dev.rexijie@gmail.com>
2022-03-02 22:41:15 +0000

    Updated Stream Processor

--- src/main/java/dev/faaji/streams/config/KafkaStreamsConfig.java
+++ src/main/java/dev/faaji/streams/config/KafkaStreamsConfig.java
@@ -2,6 +2,7 @@ package dev.faaji.streams.config;
 
 import dev.faaji.streams.api.v1.domain.PartyModificationEvent;
 import dev.faaji.streams.api.v1.domain.ValentineUserRegistration;
+import dev.faaji.streams.model.PaymentUpdateEvent;
 import org.apache.kafka.common.serialization.Serde;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,5 +19,10 @@ public class KafkaStreamsConfig {
     @Bean
     public static Serde<ValentineUserRegistration> UserRegSerde() {
         return new JsonSerde<>(ValentineUserRegistration.class);
+    }
+
+    @Bean
+    public static Serde<PaymentUpdateEvent> PaymentUpdateSerde() {
+        return new JsonSerde<>(PaymentUpdateEvent.class);
     }
 }
