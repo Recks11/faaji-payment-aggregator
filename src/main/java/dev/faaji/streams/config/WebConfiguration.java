744d03443b1b1b47ba6737fa96cc72b6a192c211
Rex Ijiekhuamen <dev.rexijie@gmail.com>
2022-03-12 01:55:28 +0000

    added function definition

--- src/main/java/dev/faaji/streams/config/WebConfiguration.java
+++ src/main/java/dev/faaji/streams/config/WebConfiguration.java
@@ -1,7 +1,5 @@
 package dev.faaji.streams.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
